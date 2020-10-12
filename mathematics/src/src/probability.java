d4fb4833bcd4b8095e2c8b3d136b9c2a92a353cb
FireRacing <kallamtarun5@gmail.com>
2020-10-12 10:16:44 +0530

    Added bayes_theorem method

--- mathematics/src/src/probability.java
+++ mathematics/src/src/probability.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class probability{
 	Scanner in = new Scanner(System.in);
 	tools1 t = new tools1();
-	public double conditional_probab(int set1[], int set2[])
+	public double[] conditional_probab(int set1[], int set2[])
     /*
      * Computes conditional probability for
      * a set
      */
     {
-    	double result = 0;
+    	double result[] = new double[2];
     	String operations;
     	System.out.println("What operations do you wish to perform on the string? ");
     	operations = in.nextLine();
@@ -87,7 +87,27 @@ public class probability{
     	 * |
     	 * v
     	 */
-    	result = (result1.size()/set2.length)/result2;
+    	result[0] = (result1.size()/set2.length)/result2;
+    	result[1] = result2;
     	return result;
     }
+	public double bayes_theorem(int[] set1, int[] set2)
+	{
+		double result = 0;
+    	double[] result3 =  conditional_probab(set1, set2);
+    	System.out.println("is there any other condtion you wish to check for?");
+    	if(in.nextLine() == "no")
+    	{
+    		/*
+    		 * Computes P(B)
+    		 * |
+    		 * |
+    		 * |
+    		 * v
+    		 */
+    		double result2 =  1-result3[1];
+    		result = (result3[0]*result3[1])/result2; // Computes P(B/A)
+    	}
+    	return result;
+	}
 }
